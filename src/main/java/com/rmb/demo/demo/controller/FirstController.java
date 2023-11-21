2c233d83513bfd8f6f1576df3f0a028279227347
manibharathi <manibharathir@sugunafoods.com>
2023-11-21 15:25:16 +0530

    Initial Commit

---
+++ src/main/java/com/rmb/demo/demo/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.rmb.demo.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+
+
+}
