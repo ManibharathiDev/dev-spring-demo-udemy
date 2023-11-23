ba163d38ebb1ceda075816d14aa3532a19b1f459
manibharathi <manibharathir@sugunafoods.com>
2023-11-23 13:53:33 +0530

    Setter Injection Added

--- src/main/java/com/rmb/demo/demo/DemoApplication.java
+++ src/main/java/com/rmb/demo/demo/DemoApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-		scanBasePackages = {"com.rmb.demo.demo","com.rmb.util"
-		}
+        scanBasePackages = {"com.rmb.demo.demo", "com.rmb.util"
+        }
 )
 public class DemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
-	}
+    }
 
 }
